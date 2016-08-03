313736d2e579b842d4e56a8ba34b42ec0dc4c251
Lev Aronsky <aronsky@gmail.com>
2016-08-03 18:14:31 +0300

    Multiple changes. Bottom line is, now the script can be run without modifications both from the command line (and be used to print the function symbols in a format that radare2 understands), and also be run, unmodified, via IDA's Script command (Alt+F7).

--- ida-symbol-transfer.py
+++ ida-symbol-transfer.py
@@ -1,6 +1,7 @@
-import idc
-import idautils
+import argparse
 import pickle
+import json
+import sys
 
 class Function(dict):
     def __init__(self, ea):
@@ -10,6 +11,9 @@ class Function(dict):
         # Name (if user-defined)
         self._name = idc.GetFunctionName(ea)
 
+        # Function end
+        self._end = idc.GetFunctionAttr(ea, idc.FUNCATTR_END)
+
         # Flags
         self._flags = idc.GetFunctionFlags(ea)
 
@@ -18,52 +22,80 @@ class Function(dict):
         self._non_rep_cmt = idc.GetFunctionCmt(ea, False)
     
 
-    @classmethod
-    def ExportFunctions(cls):
-        functions = [Function(addr) for addr in idautils.Functions(idc.MinEA(), idc.MaxEA())]
+def ExportFunctionsFromIDA(outfilename):
+    if not outfilename:
+        idc.Message("No file selected!\n")
+        return
 
-        if not functions:
-            idc.Message("No functions found!\n")
-            return
+    functions = [Function(addr) for addr in idautils.Functions(MinEA(), MaxEA())]
 
-        outfilename = idc.AskFile(1, "*.pkl", "Export functions to file...") # 1 = Save
+    if not functions:
+        idc.Message("No functions found!\n")
+        return
 
-        if not outfilename:
-            idc.Message("No file selected!\n")
-            return
+    with open(outfilename, "wb") as outfile:
+        pickle.dump(functions, outfile)
 
-        with open(outfilename, "wb") as outfile:
-            pickle.dump(functions, outfile)
+    idc.Message("Exported %d functions to %s!\n" % (len(functions), outfilename))
 
-        idc.Message("Exported %d functions to %s!\n" % (len(functions), outfilename))
+def ImportFunctionsIntoIDA(infilename):
+    if not infilename:
+        idc.Message("No file selected!\n")
+        return
 
-    @classmethod
-    def ImportFunctions(cls):
-        infilename = idc.AskFile(0, "*.pkl", "Import functions from file...") # 0 = Open
+    with open(infilename, "rb") as infile:
+        functions = pickle.load(infile)
 
-        if not infilename:
-            idc.Message("No file selected!\n")
-            return
+    for function in functions:
+        idc.MakeFunction(function._addr)
 
-        with open(infilename, "rb") as infile:
-            functions = pickle.load(infile)
+        if not function._name.startswith("sub_") and not function._name.startswith("nullsub_"):
+            idc.MakeName(function._addr, str(function._name))
 
-        for function in functions:
-            idc.MakeFunction(function._addr)
+        idc.SetFunctionFlags(function._addr, function._flags)
+        idc.SetFunctionCmt(function._addr, str(function._rep_cmt), True)
+        idc.SetFunctionCmt(function._addr, str(function._non_rep_cmt), False)
 
-            if not function._name.startswith("sub_") and not function._name.startswith("nullsub_"):
-                idc.MakeName(function._addr, str(function._name))
+    idc.Message("Imported %d functions from %s!\n" % (len(functions), infilename))
 
-            idc.SetFunctionFlags(function._addr, function._flags)
-            idc.SetFunctionCmt(function._addr, str(function._rep_cmt), True)
-            idc.SetFunctionCmt(function._addr, str(function._non_rep_cmt), False)
+def PrintFunctionsInR2Format(infilename):
+    with open(infilename, "rb") as infile:
+        functions = pickle.load(infile)
+    
+    for function in functions:
+        if not function._name.startswith("nullsub_"):
+            if function._name.startswith("sub_"):
+                function._name = function._name.replace("sub_", "fcn.")
+            #print "afn %s 0x%x" % (function._name, function._addr)
+            #function._name = "sym." + function._name
+            print "f+%s %d @ 0x%x" % (function._name, function._end - function._addr, function._addr)
+            print "af+ 0x%x 0x%x %s" % (function._addr, function._end - function._addr, function._name)
+            print "afb+ 0x%x 0x%x 0x%x" % (function._addr, function._addr, function._end - function._addr)
+            
+def command_line_mode():
+    parser = argparse.ArgumentParser(description="Parse pickled/JSONed IDB files")
+    parser.add_argument('-p', '--pickle', required=True, help="The name of the pickled IDB file", metavar="pickle_filename")
+    arguments = parser.parse_args()
+    if arguments.pickle:
+        PrintFunctionsInR2Format(arguments.pickle)
+    else:
+        parser.print_usage()
+        sys.exit(1)
 
-        idc.Message("Imported %d functions from %s!\n" % (len(functions), infilename))
+def ida_script_mode():
+    userResponse = AskYN(1, "Export (Yes) / Import (No) / Do nothing (Cancel)")
+    if 1 == userResponse:
+        ExportFunctionsFromIDA(AskFile(1, "*.pkl;*.json", "Export functions to file..."))
+    elif 0 == userResponse:
+        ImportFunctionsIntoIDA(AskFile(0, "*.pkl;*.json", "Import functions from file..."))
 
+if __name__ == '__main__':
+    try:
+        import idautils
+        import idc
 
-# User interaction code:
-userResponse = idc.AskYN(1, "Export (Yes) / Import (No) / Do nothing (Cancel)")
-if 1 == userResponse:
-    Function.ExportFunctions()
-elif 0 == userResponse:
-    Function.ImportFunctions()
+        # Code was run as a script in IDA:
+        ida_script_mode()
+    except ImportError, ie:
+        # Code was run from command line.
+        command_line_mode()
